9c1077e14ccba730827693680ed426511534446e
StCost <kostya010511@gmail.com>
2026-06-08 17:28:40 +0300

    Update game preview images and remove obsolete PNG assets
    - Changed image file extensions from PNG to JPG for various game previews in the extras manifest.
    - Deleted the Python script used for capturing game preview PNGs, as it is no longer needed.
    - Removed outdated PNG assets for multiple games, streamlining the asset directory and ensuring consistency in image formats.

--- Web Main Menu/extras-manifest.js
+++ Web Main Menu/extras-manifest.js
@@ -5,7 +5,7 @@
         id: "atom-clicker",
         title: "Atom Clicker",
         path: "Extras/games/atom-clicker/index.html",
-        image: "Extras/games/atom-clicker/atom-clicker-preview.png",
+        image: "Extras/games/atom-clicker/atom-clicker-preview.jpg",
         desktopIcon: "Extras/games/atom-clicker/atom-clicker-desktop-icon.png",
         difficulty: 1
       },
@@ -13,7 +13,7 @@
         id: "deep-collapse",
         title: "Deep Collapse",
         path: "Extras/games/deep-collapse/index.html",
-        image: "Extras/games/deep-collapse/deep-collapse-preview.png",
+        image: "Extras/games/deep-collapse/deep-collapse-preview.jpg",
         desktopIcon: "Extras/games/deep-collapse/deep-collapse-desktop-icon.png",
         difficulty: 2
       },
@@ -21,7 +21,7 @@
         id: "citrus-slice",
         title: "Citrus Slice",
         path: "Extras/games/citrus-slice/index.html",
-        image: "Extras/games/citrus-slice/citrus-slice-preview.png",
+        image: "Extras/games/citrus-slice/citrus-slice-preview.jpg",
         desktopIcon: "Extras/games/citrus-slice/citrus-slice-desktop-icon.png",
         difficulty: 2
       },
@@ -29,7 +29,7 @@
         id: "hackout",
         title: "Hackout",
         path: "Extras/games/hackout/index.html",
-        image: "Extras/games/hackout/hackout-preview.png",
+        image: "Extras/games/hackout/hackout-preview.jpg",
         desktopIcon: "Extras/games/hackout/hackout-desktop-icon.png",
         difficulty: 2
       },
@@ -37,7 +37,7 @@
         id: "last-run",
         title: "Last Run",
         path: "Extras/games/last-run/index.html",
-        image: "Extras/games/last-run/last-run-preview.png",
+        image: "Extras/games/last-run/last-run-preview.jpg",
         desktopIcon: "Extras/games/last-run/last-run-desktop-icon.png",
         difficulty: 3
       },
@@ -45,7 +45,7 @@
         id: "airborne-robot",
         title: "Airborne Robot",
         path: "Extras/games/airborne-robot/index.html",
-        image: "Extras/games/airborne-robot/airborne-robot-preview.png",
+        image: "Extras/games/airborne-robot/airborne-robot-preview.jpg",
         difficulty: 4,
         startMusicPath: "ui-robot-start-music.wav",
         gameplayMusicPath: "ui-robot-game-music.wav"
@@ -54,7 +54,7 @@
         id: "orbital-golf",
         title: "Orbital Golf",
         path: "Extras/games/orbital-golf/index.html",
-        image: "Extras/games/orbital-golf/orbital-golf-preview.png",
+        image: "Extras/games/orbital-golf/orbital-golf-preview.jpg",
         desktopIcon: "Extras/games/orbital-golf/orbital-golf-desktop-icon.png",
         difficulty: 4
       },
@@ -62,7 +62,7 @@
         id: "sector-vector",
         title: "Sector Vector",
         path: "Extras/games/sector-vector/index.html",
-        image: "Extras/games/sector-vector/sector-vector-preview.png",
+        image: "Extras/games/sector-vector/sector-vector-preview.jpg",
         desktopIcon: "Extras/games/sector-vector/sector-vector-desktop-icon.png",
         difficulty: 5,
         inputMode: "movement"
@@ -71,7 +71,7 @@
         id: "trouble-drivers",
         title: "Trouble Drivers",
         path: "Extras/games/trouble-drivers/index.html",
-        image: "Extras/games/trouble-drivers/trouble-drivers-preview.png",
+        image: "Extras/games/trouble-drivers/trouble-drivers-preview.jpg",
         desktopIcon: "Extras/games/trouble-drivers/trouble-drivers-desktop-icon.png",
         difficulty: 5,
         inputMode: "movement"
@@ -80,7 +80,7 @@
         id: "cargo-convoy",
         title: "Cargo Convoy",
         path: "Extras/games/cargo-convoy/index.html",
-        image: "Extras/games/cargo-convoy/cargo-convoy-preview.png",
+        image: "Extras/games/cargo-convoy/cargo-convoy-preview.jpg",
         desktopIcon: "Extras/games/cargo-convoy/cargo-convoy-desktop-icon.png",
         difficulty: 5,
         inputMode: "cursor"
@@ -89,7 +89,7 @@
         id: "factory-night",
         title: "Factory Night",
         path: "Extras/games/factory-night/index.html",
-        image: "Extras/games/factory-night/factory-night-preview.png",
+        image: "Extras/games/factory-night/factory-night-preview.jpg",
         desktopIcon: "Extras/games/factory-night/factory-night-desktop-icon.png",
         difficulty: 5
       },
@@ -97,7 +97,7 @@
         id: "demon-bastion",
         title: "Demon Bastion",
         path: "Extras/games/demon-bastion/index.html",
-        image: "Extras/games/demon-bastion/demon-bastion-preview.png",
+        image: "Extras/games/demon-bastion/demon-bastion-preview.jpg",
         desktopIcon: "Extras/games/demon-bastion/demon-bastion-desktop-icon.png",
         difficulty: 5,
         inputMode: "cursor"
@@ -106,7 +106,7 @@
         id: "playing-cards",
         title: "Playing Cards",
         path: "Extras/games/playing-cards/index.html",
-        image: "Extras/games/playing-cards/playing-cards-preview.png",
+        image: "Extras/games/playing-cards/playing-cards-preview.jpg",
         desktopIcon: "Extras/games/playing-cards/playing-cards-desktop-icon.png",
         difficulty: 0
       },
@@ -122,7 +122,7 @@
         id: "notepad",
         title: "Notepad",
         path: "Extras/games/notepad/index.html",
-        image: "Extras/games/notepad/notepad-preview.png",
+        image: "Extras/games/notepad/notepad-preview.jpg",
         desktopIcon: "Extras/games/notepad/notepad-desktop-icon.png",
         difficulty: 0
       },
